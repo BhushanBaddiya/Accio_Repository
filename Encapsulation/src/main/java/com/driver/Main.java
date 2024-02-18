121b6f492674efca1d11c8d86c24c3032e4abb3f
bhushan baddiya <bhushanbaddiya0@gmail.com>
2024-02-18 18:08:58 +0530

    add readme

---
+++ Encapsulation/src/main/java/com/driver/Main.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("bhushan");
+        System.out.println(obj.getName());
+
+
+    }
+}
